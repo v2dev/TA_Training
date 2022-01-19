63bdaa814ff7330243d21d792f52353a2b71cd5c
vijaymogali <96084092+vijaymogali@users.noreply.github.com>
2022-01-19 17:03:21 -0500

    changed filename

--- demoqa-spec.js
+++ demoqa-spec.js
@@ -2,8 +2,8 @@
 /// <reference types="cypress" />
 
 describe('My First Cypress Test', function () {
-  it('Visits Google', function () {
-    //Visit google Website
+  it('Visits demo QA Site', function () {
+    //Visit demoqa Website
     cy.visit('https://demoqa.com/');
     cy.get(':nth-child(4) > :nth-child(1) > .avatar > svg').click();
     //Verify number of items present under the Widget Tab
