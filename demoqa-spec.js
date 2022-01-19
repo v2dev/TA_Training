b77d6b2b54b07436c060017ad843b6228312f2c3
vijaymogali <96084092+vijaymogali@users.noreply.github.com>
2022-01-19 17:58:00 -0500

    Update demoqa-spec.js

--- demoqa-spec.js
+++ demoqa-spec.js
@@ -2,11 +2,19 @@
 /// <reference types="cypress" />
 
 describe('My First Cypress Test', function () {
-  it('Visits demo QA Site', function () {
-    //Visit demoqa Website
+  it('Visits Google', function () {
+    //Visit google Website
     cy.visit('https://demoqa.com/');
     cy.get(':nth-child(4) > :nth-child(1) > .avatar > svg').click();
     //Verify number of items present under the Widget Tab
     cy.get(':nth-child(4) > .element-list > .menu-list > li').should('have.length', 9);
+    cy.get(':nth-child(2) > .group-header > .header-wrapper').click();
+    cy.get(':nth-child(2) > .element-list > .menu-list > #item-0 > .text').click();
+    cy.get('.playgound-header').contains('Practice Form');
+    // cy.visit(Cypress.env('imago_url'));
+    cy.get('#firstName').type('vijay');
+    cy.get('#lastName').type('Mogali');
+    cy.get('#userEmail').type('abc@gmail.com');
+    cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
   });
 });
