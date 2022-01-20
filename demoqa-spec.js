df3a6caf16dce9927feb824f6d33ab842b4f87c9
vijaymogali <96084092+vijaymogali@users.noreply.github.com>
2022-01-20 16:26:29 -0500

    Update demoqa-spec.js

--- demoqa-spec.js
+++ demoqa-spec.js
@@ -1,8 +1,10 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
+import authData from '../../fixtures/auth';
 
+console.log(authData);
 describe('My First Cypress Test', function () {
-  it('Visits demoqa', function () {
+  it('Visits demoQa', function () {
     //Visit demoQA Website
     cy.visit('https://demoqa.com/');
     cy.get(':nth-child(4) > :nth-child(1) > .avatar > svg').click();
@@ -12,9 +14,20 @@ describe('My First Cypress Test', function () {
     cy.get(':nth-child(2) > .element-list > .menu-list > #item-0 > .text').click();
     cy.get('.playgound-header').contains('Practice Form');
     // cy.visit(Cypress.env('imago_url'));
-    cy.get('#firstName').type('vijay');
-    cy.get('#lastName').type('Mogali');
-    cy.get('#userEmail').type('abc@gmail.com');
+  });
+  it('Input Data - Student Registration Form', () => {
+    const { name } = authData;
+    cy.get('#firstName').type(name.firstname);
+    cy.get('#lastName').type(name.lastname);
+    cy.get('#userEmail').should('be.visible').type(name.email);
     cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
+    cy.get('#userNumber').type(name.phone);
+    // cy.get('#dateOfBirthInput').type(name.dob);
+    cy.get('.subjects-auto-complete__value-container').type(name.sub);
+    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click();
+    // cy.get('#stateCity-wrapper > :nth-child(2)').click();
+    // cy.get('[id="state"]').select('NCR');
+    // cy.get('#stateCity-wrapper > :nth-child(3)').select(Option - 2);
+    cy.get('react-select-3-input').click();
   });
 });
